1bca5438cbf6dea475279a9ada0688c164123133
Dulaj Kavinda <dulajkaavinda@gmail.com>
2021-02-10 15:35:29 +0530

    Transition Options

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,20 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-
+import React from "react";
+import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 const Home = () => {
   return (
-    <div className="home container">
-      <h2>
-        Welcome to Pizza Joint
-      </h2>
+    <motion.div
+      animate={{ opacity: 1 }}
+      initial={{ opacity: 0 }}
+      transition={{ delay: 1.5, duration: 1.5 }}
+      className="home container"
+    >
+      <motion.h2 animate={{}}>Welcome to Pizza Joint</motion.h2>
       <Link to="/base">
-        <button>
-          Create Your Pizza
-        </button>
+        <motion.button animate={{}}>Create Your Pizza</motion.button>
       </Link>
-    </div>
-  )
-}
+    </motion.div>
+  );
+};
 
 export default Home;
