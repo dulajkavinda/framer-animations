f3f7b9a7c5a96244de5e98bf66adbe4ef98f4c76
Dulaj Kavinda <dulajkaavinda@gmail.com>
2021-02-10 19:26:52 +0530

    modal done

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
-
+import { Link } from "react-router-dom";
 const backdrop = {
   active: {
     opacity: 1,
@@ -10,7 +10,19 @@ const backdrop = {
   },
 };
 
-export default function Modal({ showModal }) {
+const modal = {
+  inactive: {
+    y: "-100vw",
+    opacity: 0,
+  },
+  active: {
+    y: 100,
+    opacity: 1,
+    transition: { delay: 0.5 },
+  },
+};
+
+export default function Modal({ showModal, setShowModal }) {
   return (
     <AnimatePresence exitBeforeEnter>
       {showModal && (
@@ -19,7 +31,15 @@ export default function Modal({ showModal }) {
           animate="active"
           initial="inactive"
           className="backdrop"
-        ></motion.div>
+          exit="inactive"
+        >
+          <motion.div variants={modal} className="modal">
+            <p>Want to make another pizza?</p>
+            <Link to="/">
+              <button>Okay</button>
+            </Link>
+          </motion.div>
+        </motion.div>
       )}
     </AnimatePresence>
   );
